7eb7a04b67b20664436f5cc1755d3985d8f8db0b
massimiliano <massimarcello@hotmail.it>
2024-04-04 23:43:04 +0200

    works

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import variables from '@/app/variable.module.scss';
+
+const NavBar = () => {
+  return (
+    <div className={variables.hone}>NavBar</div>
+  )
+}
+
+export default NavBar
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
-const path = require('path')
+import { fileURLToPath } from 'url';
+import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+export default {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/images/d1s3pdmc/production/*',
       },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-     pathname: '/images/d1s3pdmc/production/*',
-        },
-      ],
-    },
-  };
+    ],
+  },
+};
